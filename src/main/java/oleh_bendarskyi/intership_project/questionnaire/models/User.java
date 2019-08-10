a96990e0cd7c43f8aa0c3152b1f737d166f244ec
Oleh Bendarskyi <oleh_bendarskyi@epam.com>
2019-08-10 22:42:00 +0300

    One more commit

--- src/main/java/oleh_bendarskyi/intership_project/questionnaire/models/User.java
+++ src/main/java/oleh_bendarskyi/intership_project/questionnaire/models/User.java
@@ -1,17 +1,33 @@
 package oleh_bendarskyi.intership_project.questionnaire.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
 
 @Data
-@ManagedBean(name = "user", eager = true)
-@SessionScoped
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "q_user", schema = "public")
 public class User {
 
+    @Id
+   @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+    @Column(name="first_name")
     private String firstName;
+    @Column(name="last_name")
     private String lastName;
+    @Column(name="phone_number")
     private String phoneNumber;
     private String email;
     private String password;
